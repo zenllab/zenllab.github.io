d886ae13ed0f6c2f15efda8d3bfec1f3c1be427b
Xiangyu QUAN <zenkogyoku@gmail.com>
2025-09-10 10:39:23 +0900

    Unknown

--- Node.js
+++ Node.js
@@ -2,15 +2,14 @@ const express = require('express');
 const Dropbox = require('dropbox').Dropbox;
 const fetch = require('isomorphic-fetch');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // ✨ cors 패키지 추가
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
 const ACCESS_TOKEN = process.env.DROPBOX_ACCESS_TOKEN;
 
-// Express 미들웨어 설정
-app.use(cors()); // ✨ CORS 미들웨어 사용
+app.use(cors());
 app.use(bodyParser.json());
 
 const dbx = new Dropbox({ accessToken: ACCESS_TOKEN, fetch: fetch });
@@ -18,7 +17,7 @@ const dbx = new Dropbox({ accessToken: ACCESS_TOKEN, fetch: fetch });
 app.post('/upload', (req, res) => {
   const reportContent = req.body.content;
   const fileName = `report_${Date.now()}.txt`;
-  
+
   dbx.filesUpload({ path: `/${fileName}`, contents: reportContent })
     .then(response => {
       res.json({ success: true, data: response });
